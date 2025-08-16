1f94f332341f84bad78cbbb6e4faa44a20c08d3a
Yash Raj <kyashraj991@gmail.com>
2025-08-16 11:17:58 +0530

    feat(UI): add input btn

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import React from 'react';
 
 const page = () => {
   return (
-    <div>Authorised</div>
-  )
-}
+    <div className="flex justify-center items-center h-screen">
+      <div className='w-[50%] flex gap-2'>
+        <Input placeholder="Enter a topic" />
+        <Button variant="outline">Autocast</Button>
+      </div>
+    </div>
+  );
+};
 
-export default page
+export default page;
